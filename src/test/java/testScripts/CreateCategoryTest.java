4312931822b3fa1c9447d42e922f11429e60db4b
anandkonam <147052048+anandkonam@users.noreply.github.com>
2023-11-09 11:23:19 +0530

    Update CreateCategoryTest.java

--- src/test/java/testScripts/CreateCategoryTest.java
+++ src/test/java/testScripts/CreateCategoryTest.java
@@ -3,7 +3,7 @@ package testScripts;
 import org.testng.annotations.Test;
 
 public class CreateCategoryTest extends BaseClass{
-	
+	//This Case is passed
 	@Test
 	public void createCategoryTest() {
 		home.clickCoursesTab();
